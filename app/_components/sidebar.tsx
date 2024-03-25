35ddba42b1b6d224348bc8259bde0e0ffeb05086
ahmad-salah94 <salah94.sy@gmail.com>
2024-03-25 11:21:29 +0100

    mlops website

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,16 +1,16 @@
-import { FileStack, Home, Package } from "lucide-react"
+import { FileStack, Home, Package, Component, Notebook, Wrench, Percent, Castle } from "lucide-react"
 import Link from "next/link"
 
 export const Sidebar = () => {
     return (
-        <div className="flex flex-col items-center w-64 h-full pt-4 overflow-hidden text-gray-700 bg-gray-100 rounded">
+        <div className="flex flex-col items-center w-64 h-screen pt-4 overflow-hidden text-gray-700 bg-gray-100 rounded">
             <Link href="/" className="flex items-center w-full px-3 mt-3">
-                <span className="ml-2 text-sm font-bold">Stack Builder</span>
+                <span className="ml-2 text-sm font-bold">MLOps</span>
             </Link>
 
             <div className="w-full px-2">
                 <div className="flex flex-col items-center w-full mt-3 border-t border-gray-300">
-                    <Link href="/" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
+                    <Link href="/home" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
                         <Home />
                         <span className="ml-2 text-sm font-medium">Home</span>
                     </Link>
@@ -18,13 +18,25 @@ export const Sidebar = () => {
                         <FileStack />
                         <span className="ml-2 text-sm font-medium">Stack Builder</span>
                     </Link>
-                    <Link href="/secondpage" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
-                        <Package />
-                        <span className="ml-2 text-sm font-medium">Second Page</span>
+                    <Link href="/component" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
+                        <Component />
+                        <span className="ml-2 text-sm font-medium">MLOps technical components</span>
                     </Link>
-                    <Link href="/thirdpage" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
-                        <Package />
-                        <span className="ml-2 text-sm font-medium">Third Page</span>
+                    <Link href="/roles" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
+                        <Notebook />
+                        <span className="ml-2 text-sm font-medium">MLOps Roles</span>
+                    </Link>
+                    <Link href="/tools" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
+                        <Wrench />
+                        <span className="ml-2 text-sm font-medium">MLOps Tools</span>
+                    </Link>
+                    <Link href="/maturity-model" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
+                        <Percent />
+                        <span className="ml-2 text-sm font-medium">MLOps Maturity Model</span>
+                    </Link>
+                    <Link href="/reference-architecture" className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
+                        <Castle />
+                        <span className="ml-2 text-sm font-medium">MLOps Reference-Architecture</span>
                     </Link>
                 </div>
             </div>

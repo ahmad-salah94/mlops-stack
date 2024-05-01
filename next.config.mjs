6383b92a771f1ab14f50ce3907cea730d7ae83e5
ahmad-salah94 <salah94.sy@gmail.com>
2024-05-01 17:10:44 +0200

    fix bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 import BuilderDevTools from "@builder.io/dev-tools/next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = BuilderDevTools()({});
+const nextConfig = BuilderDevTools()({
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // Setze dies auf `false`, falls es eine temporäre Umleitung sein soll
+      },
+    ];
+  },
+});
 
 export default nextConfig;
